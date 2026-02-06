6977fed86b681046ea8a63801ea569c06e6a3d97
gustavoweb <contato@gustavoweb.com>
2026-02-06 10:14:58 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="auth-container">
+        <div className="auth-background">
+          <div className="gradient-orb orb-1"></div>
+          <div className="gradient-orb orb-2"></div>
+          <div className="gradient-orb orb-3"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
